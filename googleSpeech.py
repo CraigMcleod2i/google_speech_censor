5fc48f8d937f401cf7b0b5096ba0ec260fb5f870
Craig McLeod <craig.mcleod@2itesting.com>
2021-05-19 14:42:55 +0100

    Initial commit

---
+++ googleSpeech.py
@@ -0,0 +1,35 @@
+from google.cloud import vision, speech
+
+def transcribe_file_censored(speech_file, censored_words):
+    """Transcribe the given audio file."""
+    
+    import io
+
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        # sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+
+    for result in response.results:   
+        response = u"{}".format(result.alternatives[0].transcript).split()
+        string = ' '
+
+        for i, word in enumerate(response):
+            for j, bleep in enumerate(censored_words):
+                bleep
+                if response[i].lower() == censored_words[j].lower():
+                    response[i] = '*' * len(word)
+        print(string.join(response))
+
+transcribe_file_censored('meeting.wav', ['it', 'as', 'he', 'she', 'paper', 'round', 'plan', 'act'])
